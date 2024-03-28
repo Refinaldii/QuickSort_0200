e9c28bca88d8ae8884a5b0108c359f6856795043
Refinaldii <refinaldi9786@gmail.com>
2024-03-28 10:55:23 +0700

    menambahkan  validasi untuk array tidak melebihi 20

--- QuickSort_0200.cpp
+++ QuickSort_0200.cpp
@@ -8,4 +8,8 @@ void input() {
     while (true) {
         cout << "Masukkan panjang elemen array :";
         cin >> n;
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
     }
